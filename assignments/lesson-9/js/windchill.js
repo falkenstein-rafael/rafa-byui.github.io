041a63dc8bb6fc0277b4594ba4b1b07385b2d2cc
rafa <Enter your email address>
2018-11-18 00:08:27 -0300

    Added homepage

---
+++ assignments/lesson-9/js/windchill.js
@@ -0,0 +1,10 @@
+// Input - get temperature and wind speed
+let temp = parseInt(document.getElementById("temp").innerHTML);
+let windSpeed = parseInt(document.getElementById("windSpeed").innerHTML);
+
+// Processing - Calculate chill wind factor and round
+let chillFactor = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed,0.16);
+chillFactor = Math.round(chillFactor * 10) / 10;
+// Output - chill wind factor
+document.getElementById("windChill").innerHTML =
+chillFactor;
